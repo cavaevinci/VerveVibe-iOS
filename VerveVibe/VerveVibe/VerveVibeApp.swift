f693fb2764d0bad7d4edfa4002c513048eb1fa01
cavaevinci <ivanevacicweb+private@gmail.com>
2024-03-19 21:19:26 +0100

    init xcode swiftui project

---
+++ VerveVibe/VerveVibe/VerveVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerveVibeApp.swift
+//  VerveVibe
+//
+//  Created by Nino on 19.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct VerveVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
