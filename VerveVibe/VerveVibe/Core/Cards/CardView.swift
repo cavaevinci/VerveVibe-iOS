a9f37e0e2a07183f735fa6ca4f8367e6fe165d10
cavaevinci <ivanevacicweb+private@gmail.com>
2024-03-20 13:39:26 +0100

    added swiping logic

--- VerveVibe/VerveVibe/Core/Cards/CardView.swift
+++ VerveVibe/VerveVibe/Core/Cards/CardView.swift
@@ -9,20 +9,53 @@ import SwiftUI
 
 struct CardView: View {
     
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: CGFloat = 0
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Image("MockImage")
                 .resizable()
                 .scaledToFill()
-                .frame(width: cardWidth, height: cardHeight)
-                .clipShape(RoundedRectangle(cornerRadius:10))
             UserInfoView()
                 .padding(.horizontal)
         }
+        .frame(width: cardWidth, height: cardHeight)
+        .clipShape(RoundedRectangle(cornerRadius:10))
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged)
+                .onEnded(onDragEnded)
+        )
     }
 }
 
 private extension CardView {
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+        //use abs to remove negative or possitive
+            //like -200 or 200
+        if abs(width) <= abs(screenCutoff) {
+            xOffset = 0
+            degrees = 0
+        }
+    }
+    
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+}
+
+private extension CardView {
+    
+    private var screenCutoff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
+    }
+    
     private var cardWidth: CGFloat {
         UIScreen.main.bounds.width-20
     }
