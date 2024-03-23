c968c35e1a201febeefaa1bf01625d3185072120
cavaevinci <ivanevacicweb+private@gmail.com>
2024-03-23 14:12:14 +0100

    match view ui

---
+++ VerveVibe/VerveVibe/Core/Matches/View/UserMatchView.swift
@@ -0,0 +1,82 @@
+//
+//  UserMatchView.swift
+//  VerveVibe
+//
+//  Created by Nino on 23.03.2024..
+//
+
+import SwiftUI
+
+struct UserMatchView: View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.black.opacity(0.7))
+                .ignoresSafeArea()
+            
+            VStack(spacing: 120) {
+                VStack {
+                    Image("photo")
+                    Text("You and Kelly have liked each other.")
+                        .foregroundStyle(.white)
+                }
+                HStack(spacing: 16) {
+                    Image(MockData.users[0].profileImageURLs[0])
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(.white, lineWidth: 2)
+                                .shadow(radius: 4)
+                        }
+                    Image(MockData.users[1].profileImageURLs[0])
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(.white, lineWidth: 2)
+                                .shadow(radius: 4)
+                        }
+                }
+                
+                VStack(spacing: 16) {
+                    Button("Send Message") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 350, height: 44)
+                    .background(.primary)
+                    .clipShape(Capsule())
+                    
+                    Button("Keep Swiping") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 350, height: 44)
+                    .background(.clear)
+                    .clipShape(Capsule())
+                    .overlay {
+                        Capsule()
+                            .stroke(.white, lineWidth: 1)
+                            .shadow(radius: 4)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    UserMatchView(show: .constant(true))
+}
